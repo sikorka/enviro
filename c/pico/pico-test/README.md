112d74fce32582e0541fb31bed339f7d8f72d3d6
An <an@sikorka.eu>
2024-08-10 13:30:51 +0200

    getting Pico serial number, C code working on Pico (#4)
    * network stuff
    
    * trying to get Pico/Enviro+ results onto internet
    
    * Adding readme
    
    * added an infinite loop of printing
    
    * looping hello
    
    * tested
    
    * printing serial number done

---
+++ c/pico/pico-test/README.md
@@ -0,0 +1,3 @@
+In order to start coding in C on Raspberry Pi Pico go through this amazing tutorial https://blog.smittytone.net/2021/02/02/program-raspberry-pi-pico-c-mac/. 
+
+Also use the reference guide https://datasheets.raspberrypi.com/pico/raspberry-pi-pico-c-sdk.pdf for library names and APIs. 
--- c/pico/pico-test/serialnumber/source.c
+++ c/pico/pico-test/serialnumber/source.c
@@ -8,16 +8,16 @@ int main(int argc, char const *argv[])
     stdio_init_all();
 
     int len = 2 * PICO_UNIQUE_BOARD_ID_SIZE_BYTES + 1;
-    uint8_t buff[len];
-    pico_get_unique_board_id_string((char *)buff, len);
+    uint8_t serialno[len];
+    pico_get_unique_board_id_string((char *)serialno, len);
 
-    printf("Printing Pico serial number: ");
-    
     while (true)
     {
-        printf(buff);
-        sleep_ms(1000);
+        printf("Printing Pico serial number: ");
+        printf(serialno);
+        printf("\n");
+        sleep_ms(5000);
     }
-
+    
     return 0;
 }
